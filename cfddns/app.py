cc0697087cfbeee80e5a3e733847874826f02957
alvaro <garciajaenalvaro@gmail.com>
2022-08-17 21:35:03 +0200

    add logging

--- cfddns/app.py
+++ cfddns/app.py
@@ -3,6 +3,17 @@ import CloudFlare
 import os
 import sys
 import click
+import logging
+
+def config_logging(filename):
+    logging.basicConfig(
+        filename = filename,
+        filemode = "a",
+        level = logging.INFO,
+        format = '%(asctime)s - %(levelname)s: %(message)s',
+        datefmt = '%Y-%m-%d %H:%M:%S'
+    )
+
 
 def get_ip():
     return requests.get("https://ifconfig.me").text
@@ -29,18 +40,21 @@ def update_record(zone, subdomain, ip):
     records = cf.zones.dns_records.get(zone_id, params=search_params)
     if not records:
         cf.zones.dns_records.post(zone_id, data=new_record_data)
-        print("New record created")
+        logging.info("New record created")
     else:
         record_id = records[0]["id"]
         if ip != records[0]["content"]:
             cf.zones.dns_records.put(zone_id, record_id, data=new_record_data)
-            print("Record updated")
+            logging.info("Record updated")
         else:
-            print("No need to update")
+            logging.info("No need to update")
 
 @click.command()
 @click.argument("subdomain")
 @click.option("-i", "--ip", type=str, default=get_ip(), show_default="public IP", help="Specify record IP")
-def main(subdomain, ip):
+@click.option("-f", "--logfile", type=str, default="/tmp/cfddns.log", show_default="/tmp/cfddns.log", help="Specify logfile")
+def main(subdomain, ip, logfile):
+    config_logging(logfile)
+    logging.info("Trying to update {} with IP {}".format(subdomain, ip))
     zone = ".".join(subdomain.split(".")[-2:])
     update_record(zone, subdomain, ip)
